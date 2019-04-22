92e47e36549d0a99e3be04d375ef4e8bb2ae9c17
Dupeljan Linux <dupeljan@gmail.com>
2019-04-22 12:13:44 +0300

    Add methods

--- one.py
+++ one.py
@@ -14,10 +14,59 @@ def grad_diff(f,x,y,h=1e-5):
 def norm(x):
 	return sqrt(sum( [ y*y for y in x] ))
 
-#class method_second:
-#	def solve(self,f= FUNCTION,x= X0,eps= EPS,m= M, i= 0, is_valid = False):
+class Method_second:
+	def __init__(self,f= FUNCTION,eps= EPS,m= M):
+		self.f = f
+		self.eps = eps
+		self.m = m
+
+	def solve(self,x= X0, i= 0, is_valid = False):
+		grad = grad_diff(self.f,*x)
+		print("grad",grad)
+		if norm ( grad ) < self.eps[0] or i > self.m:
+			return x, i
+
+		
+		t = self.get_step( x= x, grad= grad )#dichotomy(0,1,f= fi,delta= eps[0]/2,eps= eps[0])[0]
+		print ("t ",t)
+		
+		
+		x_new = np.array(x) - t
+		print("new x", x_new)
+		
+		valid = False
+		if norm(x_new - x) < self.eps[1] and abs(self.f(*x_new)- self.f(*x)) < self.eps[1]:
+			if is_valid:
+				return x_new, i
+			else:
+				valid = True
+		return self.solve(x= x_new,i= i+1,is_valid= valid)
+
+	def get_step(self,**kwarg):
+		print("it's virtual method")
+		return 1
+
+class Steepest_descent(Method_second):
+	def get_step(self,**kwarg):
+		fi = lambda t: self.f(*[xi-t*grad_i for (xi,grad_i) in zip(kwarg["x"],kwarg["grad"]) ])
+		t = dichotomy(0,1,f= fi,delta= self.eps[0]/2,eps= self.eps[0])[0] 
+		return np.dot(t,kwarg["grad"])
+
+class Newton(Method_second):
+	def get_step(self,**kwarg):
+		return np.dot(Hessian, kwarg["grad"])
+
+#Error here
+class Newton_Raphson(Method_second):
+	def get_step(self,**kwarg):
+		d = -1 * np.dot(Hessian, kwarg["grad"])
+		fi = lambda t: self.f(*[xi-t*d_i for (xi,d_i) in zip(kwarg["x"],d) ])
+		t = dichotomy(0,1,f= fi,delta= self.eps[0]/2,eps= self.eps[0])[0] 
+		return  -1 * t * d	
+		
 
 # Метод наискорейшего спуска
+'''
 def steepest_descent(f= FUNCTION,x= X0,eps= EPS,m= M, i= 0, is_valid = False):
 	grad = grad_diff(f,*x)
 	print("grad",grad)
@@ -37,9 +86,10 @@ def steepest_descent(f= FUNCTION,x= X0,eps= EPS,m= M, i= 0, is_valid = False):
 		else:
 			valid = True
 	return steepest_descent(f=f,x= x_new,eps= eps,m= m,i= i+1,is_valid= valid)
-
+'''
 
 def main():
-	print(steepest_descent())
+	m = Newton_Raphson()
+	print(m.solve())
 if __name__ == '__main__':
 	main()
